a832cf6fa34f43ff2d41d7008ec2d2b063b85ecb
georginskaya <georginskaya@gmail.com>
2023-02-23 22:41:32 +0100

    build

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import './App.css';
-
 import Header from './Header';
 import WeatherNow from './Weather';
 
-import DrawerContent from "./DrawerContent";
+
 
 
 function App() {
--- src/ForecastLayout.js
+++ src/ForecastLayout.js
@@ -1,5 +1,4 @@
 import React from "react"; 
-import WeatherTemperature from './WeatherTemperature';
 import "./DrawerContent.css";
 
 
